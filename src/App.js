8b86bb6057b2622e39e4e36b70f07cefae19eb99
Joan Albert Segura <jalbertsr@protonmail.ch>
2017-10-26 20:23:30 +0200

    page not found added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Main from './Main'
+import NotFound from './NotFound'
 
 class App extends Component {
   render () {
     return (
       <Switch>
         <Route exact path='/' component={Main} />
+        <Route component={NotFound} />
       </Switch>
     )
   }
---
+++ src/NotFound.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const NotFound = () => <h1> Error 404 The page you're looking for is not here. </h1>
+
+export default NotFound
