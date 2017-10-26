b304f0c9e20290e8153239cc1e62c8b98b5a553c
Joan Albert Segura <jalbertsr@protonmail.ch>
2017-10-26 20:36:59 +0200

    added comntary

--- src/PokemonCard.js
+++ src/PokemonCard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 const PokemonCard = props =>
   <div className='row'>
-    <div className='col-xs-6 col-md-3'>
+    <div className='col-md-4'>
       <a href={props.url} className='thumbnail'>
         <img src={`http://www.pokestadium.com/sprites/xy/${props.name}.gif`} alt={props.name} />
         <p>{props.name}</p>
